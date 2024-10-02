53ed3291eed5ed0671088d20ab27a1b17a3a19e8
sps61303-CSUN <Stephen.Sadlowski.218@my.csun.edu>
2024-10-02 14:32:02 -0700

    Add files via upload

--- FourChannelOscope.m
+++ FourChannelOscope.m
@@ -1,34 +1,45 @@
+function FourChannelOscope(samp_rate)
 %This program connects with GNU Radio over TCP and reads 4 complex data streams.
 %Real-time data is used to create an adustable oscilloscope plot.
 %When you are done, use the "STOP" button on the plot to halt the program.
 
-clear, clc, close all
-format short, format compact
-
-samp_rate = 1E6; %samples per second
-refresh_rate = 20; %updates per second
+%Address = '192.168.56.101'
 
 %Compute oscilloscope parameters
+refresh_rate = 20;
 SampleSize = round(samp_rate/refresh_rate);
 
-%Connect to virtual machine client
-RFclient = tcpclient("192.168.56.101",2000,"Timeout",1);
+%Ping GNU Radio Host
+Ping = evalc('!ping -c 1 192.168.56.101');
+Loss = regexp(Ping, '([0-9]*)%.*loss', 'tokens');
+if isempty(Loss) | str2double(Loss{1}{1})~=0
+    disp("Host Unreachable")
+    return
+end
+
+%Connect to host over TCP
+try
+RFclient = tcpclient('192.168.56.101',2000,'Timeout',1,'EnableTransferDelay',false);
+catch
+   disp("TCP Connection Refused.")
+   return
+end
 
 %Create Stop Button and Axis Controls
 yscale = 0; xscale = 0; Loop = 1;
 figure(1)
-Button1 = uicontrol('Style','pushbutton','Position',[0 0 60 30],'String','STOP','Callback', 'Loop = 0;');
-Button2 = uicontrol('Style','pushbutton','Position',[80 0 60 30],'String','x zoom-','Callback', 'xscale = xscale+1;');
-Button3 = uicontrol('Style','pushbutton','Position',[160 0 60 30],'String','x zoom+','Callback', 'xscale = xscale-1;');
-Button4 = uicontrol('Style','pushbutton','Position',[240 0 60 30],'String','y zoom-','Callback', 'yscale = yscale+1;');
-Button5 = uicontrol('Style','pushbutton','Position',[320 0 60 30],'String','y zoom+','Callback', 'yscale = yscale-1;');
+Button1 = uicontrol('Style','pushbutton','Position',[0 0 60 30],'String','STOP','Callback', @Stop);
+Button2 = uicontrol('Style','pushbutton','Position',[80 0 60 30],'String','x zoom-','Callback', @xpp);
+Button3 = uicontrol('Style','pushbutton','Position',[160 0 60 30],'String','x zoom+','Callback', @xmm);
+Button4 = uicontrol('Style','pushbutton','Position',[240 0 60 30],'String','y zoom-','Callback', @ypp);
+Button5 = uicontrol('Style','pushbutton','Position',[320 0 60 30],'String','y zoom+','Callback', @ymm);
 while Loop
     %Read RF data from TCP Client
     RFdata = read(RFclient,SampleSize*8,"single");
     X_t = Reshape4Channel(RFdata).';
 
     %Generate Data Window
-    Window = 1:(100*10^(xscale/8));
+    Window = 1:(200*10^(xscale/8));
     TimeWindow = (Window-1)/samp_rate;
 
     %Start graph at signal peak
@@ -41,5 +52,36 @@ while Loop
     xlim('tight')
     grid on
 end
-flush(RFclient)
-clear all
+
+% Nested callback functions for buttons:
+  function Stop(~, ~)
+    Loop = false;
+  end
+
+  function xpp(~, ~)
+    if xscale<14
+        xscale = xscale+1;
+    end
+  end
+
+  function xmm(~, ~)
+      if xscale>-10
+        xscale = xscale-1;
+      end
+  end
+
+  function ypp(~, ~)
+    yscale = yscale+1;
+  end
+
+  function ymm(~, ~)
+    yscale = yscale-1;
+  end
+end
+
+function A = Reshape4Channel(B)
+%4CHANNELRESHAPE 
+T = [1 0 0 0; 1j 0 0 0; 0 1 0 0; 0 1j 0 0; 0 0 1 0; 0 0 1j 0; 0 0 0 1; ...
+     0 0 0 1j];
+A = reshape(B,8,length(B)/8).'*T; 
+end
